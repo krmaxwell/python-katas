d8418a06bc0e7bb5d948a0098ed2ab004bc4c085
Kyle Maxwell <kmax@hey.com>
2021-11-11 20:59:42 -0600

    Support finding multiple saddlepoints

--- saddle-point/saddlepoint.py
+++ saddle-point/saddlepoint.py
@@ -3,7 +3,8 @@ def get_column(array: list, column_number: int) -> list:
     return column
 
 
-def sp(array: list) -> tuple:
+def find_saddlepoints(array: list) -> list:
+    saddlepoints = []
     for row_enum in enumerate(array):
         row_number = row_enum[0]
         row = row_enum[1]
@@ -12,4 +13,6 @@ def sp(array: list) -> tuple:
             column = get_column(array, column_number)
             value = row[column_number]
             if value == max(row) and value == min(column):
-                return tuple([row_number, column_number])
+                saddlepoints.append(tuple([row_number, column_number]))
+
+    return saddlepoints
--- saddle-point/test_saddlepoint.py
+++ saddle-point/test_saddlepoint.py
@@ -8,11 +8,19 @@ array_without_sp = [
     [5, 1, 2, 3, 4],
 ]
 
-array_with_sp = [
+array_with_single_saddlepoint = [
     [5, 5, 5, 5, 5],
     [1, 3, 1, 1, 1],
     [5, 5, 5, 5, 5],
     [1, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5],
+]
+
+array_with_two_saddlepoints = [
+    [5, 5, 5, 5, 5],
+    [1, 0, 0, 0, 0],
+    [5, 5, 5, 5, 5],
+    [1, 0, 0, 0, 0],
     [5, 5, 5, 5, 5],
 ]
 
@@ -21,7 +29,11 @@ array_small = [[1, 3, 1], [1, 2, 1], [1, 3, 1]]
 
 def test_get_column():
     assert sp.get_column(array_small, 0) == [1, 1, 1]
+    assert sp.get_column(array_without_sp, 2) == [3, 4, 5, 1, 2]
 
 
 def test_saddlepoint():
-    assert sp.sp(array_small) == (1, 1)
+    assert sp.find_saddlepoints(array_small) == [(1, 1)]
+    assert sp.find_saddlepoints(array_with_single_saddlepoint) == [(1, 1)]
+    assert sp.find_saddlepoints(array_with_two_saddlepoints) == [(1, 0), (3, 0)]
+    assert sp.find_saddlepoints(array_without_sp) == []
