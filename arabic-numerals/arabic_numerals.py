031083cc67f5c3cc72949e8c63e7fc3d234c8fdb
Kyle Maxwell <kylemxl@icloud.com>
2022-03-14 12:45:04 -0500

    Improved error handling

--- arabic-numerals/arabic_numerals.py
+++ arabic-numerals/arabic_numerals.py
@@ -1,22 +1,51 @@
 def ArabicNumeral(roman: str) -> int:
     """Convert a Roman numeral to an Arabic number"""
-    if roman not in "IVXLCDM":
-        raise ValueError(
-            "Invalid character in input. Valid characters are I,V,X,L,C,D,M."
-        )
 
-    if roman == "I":
-        return 1
-    elif roman == "X":
-        return 10
-    elif roman == "C":
-        return 100
-    elif roman == "M":
-        return 1000
-    elif roman == "V":
-        return 5
-    elif roman == "L":
-        return 50
-    elif roman == "D":
-        return 500
-    return 0
+    if type(roman) != str:
+        raise TypeError("Roman numeral must be a string")
+
+    for char in roman:
+        if char not in "IVXLCDM":
+            raise ValueError(
+                "Invalid character in input. Valid characters are I,V,X,L,C,D,M."
+            )
+    if len(roman) == 0:
+        return 0
+
+    if len(roman) == 1:
+        if roman == "I":
+            return 1
+        elif roman == "X":
+            return 10
+        elif roman == "C":
+            return 100
+        elif roman == "M":
+            return 1000
+        elif roman == "V":
+            return 5
+        elif roman == "L":
+            return 50
+        elif roman == "D":
+            return 500
+        return 0
+
+    if roman[0] == "V":
+        if roman[1] != "I":
+            raise ValueError("Invalid numeral: can't subtract auxiliary symbol.")
+    if roman[0] == "L":
+        if roman[1] == "C" or roman[1] == "M":
+            raise ValueError("Invalid numeral: can't subtract auxiliary symbol.")
+
+    # Only works for post-fixed smaller values
+    if roman[0] == "I":
+        if roman[1] == "V" or roman[1] == "X":
+            return ArabicNumeral(roman[1:]) - 1
+        return 1 + ArabicNumeral(roman[1:])
+    elif roman[0] == "X":
+        if roman[1] == "L" or roman[1] == "C":
+            return ArabicNumeral(roman[1:]) - 10
+        return 10 + ArabicNumeral(roman[1:])
+    elif roman[0] == "C":
+        if roman[1] == "D" or roman[1] == "M":
+            return ArabicNumeral(roman[1:]) - 100
+        return 100 + ArabicNumeral(roman[1:])
--- arabic-numerals/test_arabic_numerals.py
+++ arabic-numerals/test_arabic_numerals.py
@@ -20,9 +20,47 @@ class TestArabicNumerals(unittest.TestCase):
         self.assertEqual(500, ArabicNumeral("D"))
 
     def test_invalid_symbols(self):
-        """Verify that ArabicNumera() throws an exception for invalid symbols"""
+        """Verify that ArabicNumeral() throws an exception for invalid symbols"""
         with self.assertRaises(ValueError):
             ArabicNumeral("K")
 
     def test_number_following_greater_or_equal_value_is_postfixed(self):
+        """Verify that ArabicNumeral() handles numbers following greater-or-equal values"""
+        self.assertEqual(2, ArabicNumeral("II"))
+        self.assertEqual(11, ArabicNumeral("XI"))
         self.assertEqual(12, ArabicNumeral("XII"))
+        self.assertEqual(13, ArabicNumeral("XIII"))
+        self.assertEqual(110, ArabicNumeral("CX"))
+        self.assertEqual(111, ArabicNumeral("CXI"))
+        self.assertEqual(120, ArabicNumeral("CXX"))
+
+    def test_number_preceded_by_less_or_equal_value_is_prefixed(self):
+        """Verify that ArabicNumeral() handles numbers preceded by less-or-equal values"""
+        self.assertEqual(4, ArabicNumeral("IV"))
+        self.assertEqual(14, ArabicNumeral("XIV"))
+        self.assertEqual(40, ArabicNumeral("XL"))
+        self.assertEqual(90, ArabicNumeral("XC"))
+
+    def test_cant_subtract_auxiliary_symbols(self):
+        """Verify that ArabicNumeral() throws an exception for auxiliary symbols"""
+        with self.assertRaises(ValueError):
+            ArabicNumeral("VL")
+        with self.assertRaises(ValueError):
+            ArabicNumeral("LM")
+
+    def test_cant_pass_non_string(self):
+        """Verify that ArabicNumeral() throws an exception for non-strings"""
+        with self.assertRaises(TypeError):
+            ArabicNumeral(1)
+        with self.assertRaises(TypeError):
+            ArabicNumeral(1.0)
+        with self.assertRaises(TypeError):
+            ArabicNumeral(True)
+        with self.assertRaises(TypeError):
+            ArabicNumeral(None)
+        with self.assertRaises(TypeError):
+            ArabicNumeral([])
+        with self.assertRaises(TypeError):
+            ArabicNumeral({})
+        with self.assertRaises(TypeError):
+            ArabicNumeral(())
