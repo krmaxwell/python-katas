825fa8bf1e406292595f4e050e361c495df8a628
Kyle Maxwell <kylemxl@icloud.com>
2022-06-08 14:15:38 -0500

    use assert* methods

--- tests/test_parens.py
+++ tests/test_parens.py
@@ -5,50 +5,50 @@ from parens.parens import balance_check
 
 class TestParens(unittest.TestCase):
     def test_simple_pairs(self):
-        assert balance_check("()") is True
-        assert balance_check("{}") is True
-        assert balance_check("[]") is True
+        self.assertTrue(balance_check("()"))
+        self.assertTrue(balance_check("{}"))
+        self.assertTrue(balance_check("[]"))
 
     def test_single_symbols(self):
-        assert balance_check("(") is False
-        assert balance_check(")") is False
-        assert balance_check("{") is False
-        assert balance_check("}") is False
-        assert balance_check("[") is False
-        assert balance_check("]") is False
+        self.assertFalse(balance_check("("))
+        self.assertFalse(balance_check(")"))
+        self.assertFalse(balance_check("{"))
+        self.assertFalse(balance_check("}"))
+        self.assertFalse(balance_check("["))
+        self.assertFalse(balance_check("]"))
 
     def test_balanced_pairs(self):
-        assert balance_check("{{}}") is True
-        assert balance_check("{}{}") is True
-        assert balance_check("(())") is True
-        assert balance_check("()()") is True
-        assert balance_check("[[]]") is True
-        assert balance_check("[][]") is True
+        self.assertTrue(balance_check("{{}}"))
+        self.assertTrue(balance_check("{}{}"))
+        self.assertTrue(balance_check("(())"))
+        self.assertTrue(balance_check("()()"))
+        self.assertTrue(balance_check("[[]]"))
+        self.assertTrue(balance_check("[][]"))
 
     def test_unbalanced_symbols(self):
-        assert balance_check("()(") is False
-        assert balance_check("(()") is False
-        assert balance_check("((()") is False
-        assert balance_check(")()") is False
-        assert balance_check(")(") is False
-        assert balance_check("{}}") is False
-        assert balance_check("}}") is False
-        assert balance_check("][") is False
-        assert balance_check("[[[][]") is False
+        self.assertFalse(balance_check("()("))
+        self.assertFalse(balance_check("(()"))
+        self.assertFalse(balance_check("((()"))
+        self.assertFalse(balance_check(")()"))
+        self.assertFalse(balance_check(")("))
+        self.assertFalse(balance_check("{}}"))
+        self.assertFalse(balance_check("}}"))
+        self.assertFalse(balance_check("]["))
+        self.assertFalse(balance_check("[[[][]"))
 
     def test_mixed_balanced_pairs(self):
-        assert balance_check("(){}") is True
-        assert balance_check("({})") is True
-        assert balance_check("()[{}]") is True
+        self.assertTrue(balance_check("(){}"))
+        self.assertTrue(balance_check("({})"))
+        self.assertTrue(balance_check("()[{}]"))
 
     def test_mixed_unbalanced_pairs(self):
-        assert balance_check("(}{)") is False
-        assert balance_check("[]([)[") is False
-        assert balance_check("[{)]") is False
+        self.assertFalse(balance_check("(}{)"))
+        self.assertFalse(balance_check("[]([)["))
+        self.assertFalse(balance_check("[{)]"))
 
     def test_readme_cases(self):
-        assert balance_check("{{)(}}") is False
-        assert balance_check("({)}") is False
-        assert balance_check("[({})]") is True
-        assert balance_check("{}([])") is True
-        assert balance_check("{()}[[{}]]") is True
+        self.assertFalse(balance_check("{{)(}}"))
+        self.assertFalse(balance_check("({)}"))
+        self.assertTrue(balance_check("[({})]"))
+        self.assertTrue(balance_check("{}([])"))
+        self.assertTrue(balance_check("{()}[[{}]]"))
