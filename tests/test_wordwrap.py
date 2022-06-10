bcbc872202b2979cc74cf06d85106a34a5788eac
Kyle Maxwell <kylemxl@icloud.com>
2022-06-10 14:06:28 -0500

    Implement naive wrapping

--- tests/test_wordwrap.py
+++ tests/test_wordwrap.py
@@ -5,16 +5,16 @@ from word_wrap.wordwrap import wrap
 
 class TestWordWrap(unittest.TestCase):
     def test_wrap_empty_line(self):
-        assert wrap("", 100) == ""
+        self.assertEqual("", wrap("", 100))
 
 
     def test_wrap_single_line(self):
-        assert wrap("One line", 80) == "One line"
+        self.assertEqual("One line", wrap("One line", 80))
 
 
     def test_wrap_two_lines(self):
-        assert wrap("Two lines", 5) == "Two\nlines"
+        self.assertEqual("Two\nlines", wrap("Two lines", 5))
 
 
     def test_wrap_longer_line(self):
-        assert wrap("Still two lines", 12) == "Still two\nlines"
+        self.assertEqual("Still two\nlines", wrap("Still two lines", 12))
--- word_wrap/wordwrap.py
+++ word_wrap/wordwrap.py
@@ -1,4 +1,11 @@
-def wrap(original_str, n):
-    if len(original_str) <= n:
-        return original_str
-    return "\n".join(original_str.split())
+def wrap(original_str: str, n: int) -> str:
+    full_strings = []
+    words = original_str.strip().split()
+    current_line = ""
+    for word in words:
+        if len(current_line) + len(word) + 1 > n:
+            full_strings.append(current_line.strip())
+            current_line = word
+        else:
+            current_line += " " + word
+    return "\n".join(full_strings + [current_line.strip()])
